7a418f10132832ba25465665d7e49257ee6b9abd
Tekazamlaeticia <107771129+Tekazamlaeticia@users.noreply.github.com>
2023-03-08 01:06:37 +0100

    Wuerfel contructor ohne paramter

--- Wuerfel.java
+++ Wuerfel.java
@@ -1,11 +1,12 @@
 public class Wuerfel {
     int seite = 1;
 
+    public Wuerfel() {}
 
     public Wuerfel(String zufall ) {
         if(zufall == "random")     {
 
-            this.seite = (int) (Math.random() * 7);
+            this.seite = (int) (Math.random() * 7);;
 
         }
     }
@@ -18,11 +19,11 @@ public class Wuerfel {
     }
     public static void main(String[] args) {
 
-       // Wuerfel w1 = new Wuerfel();
+        Wuerfel w1 = new Wuerfel();
         Wuerfel w2 = new Wuerfel(3);
         Wuerfel w3 = new Wuerfel("random");
 
-        //System.out.println("w1"+w1.toString());
+        System.out.println("w1"+w1.toString());
         System.out.println("w2"+w2.toString());
         System.out.println("w3"+w3.toString());
     }
