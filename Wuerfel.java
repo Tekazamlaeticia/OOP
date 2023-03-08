d897ead3fb361564f82797e874f0a86f7c69d8b0
Tekazamlaeticia <107771129+Tekazamlaeticia@users.noreply.github.com>
2023-03-08 01:05:11 +0100

    Random numbers

--- Wuerfel.java
+++ Wuerfel.java
@@ -1,16 +1,32 @@
-import java.util.Random;
 public class Wuerfel {
     int seite = 1;
 
-    public Wuerfel() {}
 
     public Wuerfel(String zufall ) {
         if(zufall == "random")     {
-            Wuerfel sc = new Wuerfel();
-            this.seite = sc.nextInt(1,7);
+
+            this.seite = (int) (Math.random() * 7);
 
         }
     }
+    public Wuerfel(int zahl) {
+        this.seite =  zahl;
+    }
+    @Override
+    public String toString() {
+        return "seite=" + this.seite ;
+    }
+    public static void main(String[] args) {
+
+       // Wuerfel w1 = new Wuerfel();
+        Wuerfel w2 = new Wuerfel(3);
+        Wuerfel w3 = new Wuerfel("random");
+
+        //System.out.println("w1"+w1.toString());
+        System.out.println("w2"+w2.toString());
+        System.out.println("w3"+w3.toString());
+    }
+
 
 
 }
